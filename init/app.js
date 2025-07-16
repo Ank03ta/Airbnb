19699a9ad08d8d1dd0cc7c4559796d12d05f2810
Ankita Yadav <ankitayadav0312@gmail.com>
2025-07-17 01:10:12 +0530

    Update app.js

--- init/app.js
+++ init/app.js
@@ -1,29 +1,13 @@
-const mongoose = require("mongoose");
 const Listing = require("../models/listing.js");
 const initData = require("./data.js");
-
-const mongo_URL = "mongodb://127.0.0.1:27017/wonderlust";
-
-main().then(()=>
-{
-   // console.log("connected to db succsefully");
-})
-.catch((err)=>
-{
-    console.log(err);
-})
-
-async function main() {
-    await mongoose.connect(mongo_URL);
-}
 
 const initDB = async () =>
 {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj, owner:"6862e7d6c3d83908507cdded" }));
+    initData.data = initData.data.map((obj) => ({...obj, owner:"6877f86cac204b653a7d444d" }));
     await Listing.insertMany(initData.data);
     console.log("Initialised succesfully");
 }
 
 
-// initDB();
+ //initDB();
